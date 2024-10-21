498f88db6aa0431aa8f633bc6e146662aafe7b93
milestarricone <milestarricone2@gmail.com>
2024-10-21 16:58:57 -0400

    built game framework, started defining the player turn

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ const prompt = require('prompt-sync')();
 
 let racetype
 let weapontype
-
-
-const username = prompt('What is your name? ');
-console.log(`Your name is ${username}.`);
-
-
-
+let playeLife = 10
+let computerLife = 10
 
 //This is selecting the race. it takes the users input, and checks it agains 3 predetermained entries.
 // if the user enters something that does not work, we call the function back at the bottom
-console.log("what race do you want to choose?");
 pickRace = () => {
     const raceChoice = prompt(`Please select Human, Elf, or Dwarf. `)
     if (raceChoice === `Human`) {
@@ -29,20 +23,9 @@ pickRace = () => {
         pickRace()
     }
 }
-pickRace()
-console.log(`Your chosen race is ${racetype}.`);
 
-
-
-
-
-
-
-
-
-//sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
 //This function is to take imput from the user, and pick a weapon from 3 predetermained weapon choices. 
-console.log(`what weapons do you want to choose?`)
+//sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
 pickWeapon = () => {
     const weaponChoice = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`) //asking the user what they would like to pick
     if (weaponChoice === `1`) { //comparing the choice the user made to the predetermained selection
@@ -59,16 +42,29 @@ pickWeapon = () => {
     pickWeapon()//call the function again. 
     
 }
-pickWeapon()
+
+
+const username = prompt('What is your name? ');
+console.log(`Your name is ${username}.`);
+
+console.log("what race do you want to choose?");
+pickRace()//calling the pick race function from the top of the page
+console.log(`Your chosen race is ${racetype}.`);
+
+console.log(`what weapons do you want to choose?`)
+pickWeapon()//calling the pick weapon function from the top of the page. 
 console.log(`Your choice of weapon is ${weapontype}`);
 
 
 
-
+//character sheet
 const character = { 
     name:`${username}`, 
     race:`${racetype}`,
     weapon: `${weapontype}`,
+    backpack: [flint]
+    
+    
     // weaponChoice: () => {        Tried to put this as part of a method but it did not work.
     //     if (character.weapontype = 1) {
     //         console.log(`sword and shield`);
@@ -79,11 +75,59 @@ const character = {
     //     else if (character.weapontype = 3) {
     //         console.log(`bow and arrow`);
     //     }   
-    // }, 
+    // },
     
     
 }
 
+
 // console.log(`Your weapon is ` +  ${character.weapon});
-    console.log(character );
+    console.log(character );    
     
+
+
+
+// Start of game framework
+
+//declare values
+
+//attack and heal functions
+
+//player turn
+playerturn = () => {
+    console.log(`player life is ${playeLife} and computer life is ${computerLife}`)
+
+    const playerTurnChoice = prompt(`tbd`)//todo
+        if (playerTurnChoice === `attack`) {
+            //attack function lose health from computer
+            if (weaponChoice === `1`) {
+                computerLife -= 1 //sword and shield extra defense for the shield 1 attack 2 defence
+            }
+            else if (weaponChoice === `2`) {
+                computerLife -= 3 // batte axe all attack 3 attack
+            }
+            else if (weaponChoice === `3`) {
+                computerLife -= 2 //bow gets extra defense due to mobility 2 attack 1 defence
+            }
+        }
+        else if (playerTurnChoice === `heal`) {
+            //player heal function add health to player 
+        }
+        else {
+            console.log(`invalid choice please choose better!`)
+            playerturn()
+        }      
+
+    console.log(`player life is ${playeLife} and computer life is ${computerLife}`)
+}
+computerturn = () => {
+    //todo with the random math.
+
+    //computer gets to pick attack or heal
+}
+
+
+
+
+
+//computer turn
