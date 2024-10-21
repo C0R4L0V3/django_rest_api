528a4bfb7dbf5e4dafb98b1134c53de46a5813d7
milestarricone <milestarricone2@gmail.com>
2024-10-21 16:10:38 -0400

    added functions for choosing weapons and race

--- app.js
+++ app.js
@@ -1,37 +1,89 @@
 const prompt = require('prompt-sync')();
+
+let racetype
+let weapontype
+
+
 const username = prompt('What is your name? ');
 console.log(`Your name is ${username}.`);
 
+
+
+
+//This is selecting the race. it takes the users input, and checks it agains 3 predetermained entries.
+// if the user enters something that does not work, we call the function back at the bottom
 console.log("what race do you want to choose?");
-const race = prompt(`Please select human elf and dwarf. `)
-console.log(`Your chosen race is ${race}.`);
+pickRace = () => {
+    const raceChoice = prompt(`Please select Human, Elf, or Dwarf. `)
+    if (raceChoice === `Human`) {
+        racetype = `Human`
+    }
+    else if (raceChoice === `Elf`) {
+        racetype = `Elf`
+    }
+    else if (raceChoice === `Dwarf`) {
+        racetype = `Dwarf`
+    }
+    else {
+        console.log(`invalid selection, choose better`)
+        pickRace()
+    }
+}
+pickRace()
+console.log(`Your chosen race is ${racetype}.`);
+
+
+
+
+
+
+
+
+
+//sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
 
 console.log(`what weapons do you want to choose?`)
-const weapon = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`)
-//sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
+pickWeapon = () => {
+    const weaponChoice = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`)
+    if (weaponChoice === `1`) {
+        weapontype = `Sword and Shield`
+    }
+    else if (weaponChoice === `2`) {
+        weapontype = `Battle Axe`
+    }
+    else if (weaponChoice === `3`) {
+        weapontype = `Bow and Arrow`
+    }
+    else {console.log(`invalid choice, choose better`);
+    weaponChoice}
+    pickWeapon()
+    
+}
+pickWeapon()
+console.log(`Your choice of weapon is ${weapontype}`);
 
 
 
 
 const character = { 
     name:`${username}`, 
-    race:`${race}`,
-    weapontype: `${weapon}`,
-    weaponChoice: () => {
-        if (character.weapontype = 1) {
-            console.log(`sword and shield`);
-        }
-        else if (character.weapontype = 2) {
-            console.log(`battle axe`); 
-        }
-        else if (character.weapontype = 3) {
-            console.log(`bow and arrow`);
-        }   
-    }, 
+    race:`${racetype}`,
+    weapon: `${weapontype}`,
+    // weaponChoice: () => {        Tried to put this as part of a method but it did not work.
+    //     if (character.weapontype = 1) {
+    //         console.log(`sword and shield`);
+    //     }
+    //     else if (character.weapontype = 2) {
+    //         console.log(`battle axe`); 
+    //     }
+    //     else if (character.weapontype = 3) {
+    //         console.log(`bow and arrow`);
+    //     }   
+    // }, 
     
     
 }
 
-console.log(`Your weapon is ` +  character.weaponChoice());
+// console.log(`Your weapon is ` +  ${character.weapon});
     console.log(character );
     
