46b9ddb98107af24f8677f3f36f4913a688e95b9
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-21 17:38:02 -0500

    added an init function to propperly add values to character object

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const prompt = require('prompt-sync')();
 
+const character = { 
+    name: null, 
+    race: null,
+    armor: null,
+    weapon: null,
+    attack:  0,
+    defense: 0,
+    backpack: {
+        potions: null, //<<--- we'll add a quantity as we expand
+        arrows: null, //<<--- we'll add a quantity as we eqpand
+
+    }
+}   
+
 let racetype
 let weapontype
 let playeLife = 10
 let computerLife = 10
 
+
+
 //This is selecting the race. it takes the users input, and checks it agains 3 predetermained entries.
 // if the user enters something that does not work, we call the function back at the bottom
-pickRace = () => {
+const pickRace = () => {
     const raceChoice = prompt(`Please select Human, Elf, or Dwarf. `)
     if (raceChoice === `Human`) {
         racetype = `Human`
+        character.race = raceChoice
     }
     else if (raceChoice === `Elf`) {
         racetype = `Elf`
+        character.race = raceChoice
     }
     else if (raceChoice === `Dwarf`) {
         racetype = `Dwarf`
+        character.race = raceChoice
     }
     else {
         console.log(`invalid selection, choose better`)
@@ -26,64 +45,86 @@ pickRace = () => {
 
 //This function is to take imput from the user, and pick a weapon from 3 predetermained weapon choices. 
 //sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
-pickWeapon = () => {
-    const weaponChoice = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`) //asking the user what they would like to pick
-    if (weaponChoice === `1`) { //comparing the choice the user made to the predetermained selection
+ const pickWeapon = () => {
+    const weaponChoice = prompt(`1 - Sword amd Shield,  2 - Battle Axe, 3 - Bow and Arrow `) //asking the user what they would like to pick
+    if (weaponChoice === `1`) { //comparing the choice the user made to the predetermained selection 
         weapontype = `Sword and Shield`//assigning the weapontype
+        character.attack += 1
+        character.defense += 2
     }
-    else if (weaponChoice === `2`) {//comparing the choice the user made to the predetermained selection
-        weapontype = `Battle Axe`//assigning the weapontype
+     if (weaponChoice === `2`) {  //comparing the choice the user made to the predetermained selection 
+        weapontype = `Battle Axe`   //assigning the weapontype
+        character.attack += 3
     }
-    else if (weaponChoice === `3`) {//comparing the choice the user made to the predetermained selection
-        weapontype = `Bow and Arrow`//assigning the weapontype
+     if (weaponChoice === `3`) {   //comparing the choice the user made to the predetermained selection 
+        weapontype = `Bow and Arrow` //assigning the weapontype
+        character.attack += 2
+        character.defense += 1
     }
-    else {console.log(`invalid choice, choose better`);//if the user did not pick an option that is predetermained, this calls the function again. 
-    weaponChoice}
+        else {console.log(`invalid choice, choose better`)//if the user did not pick an option that is predetermained, this calls the function again. 
     pickWeapon()//call the function again. 
-    
-}
+    }
+};
 
 
-const username = prompt('What is your name? ');
-console.log(`Your name is ${username}.`);
+const init = () => {
+    // pickName()
+    console.log("what race do you want to choose?");
+    pickRace()
+    console.log(`Your chosen race is ${racetype}.`);
+    console.log(`what weapons do you want to choose?`)
+    pickWeapon()
+    console.log(`You have choosen ${weapontype} for your weapn.`);
+    console.log(character)
+} 
 
-console.log("what race do you want to choose?");
-pickRace()//calling the pick race function from the top of the page
-console.log(`Your chosen race is ${racetype}.`);
-
-console.log(`what weapons do you want to choose?`)
-pickWeapon()//calling the pick weapon function from the top of the page. 
-console.log(`Your choice of weapon is ${weapontype}`);
+init()
 
 
+
+// ================================================================ Put all this code in a init Function==================
+
+//===We'll add the armor choice function here.=== -CL
+// pickArmor = () => {
+
+// }
+
+
+
+// const username = prompt('What is your name? ');
+// console.log(`Your name is ${username}.`);
+
+// console.log("what race do you want to choose?");
+// pickRace()//calling the pick race function from the top of the page
+// console.log(`Your chosen race is ${racetype}.`);
+
+// console.log(`what weapons do you want to choose?`)
+// pickWeapon()//calling the pick weapon function from the top of the page. 
+// console.log(`You have choosen ${weapontype} for your weapn.`);
+
+//===envoke Armor function here=== 
+
+// console.log(`Your have choose ${armortype} for your aromor.`);
 
 //character sheet
-const character = { 
-    name:`${username}`, 
-    race:`${racetype}`,
-    weapon: `${weapontype}`,
-    backpack: [flint]
-    
-    
-    // weaponChoice: () => {        Tried to put this as part of a method but it did not work.
-    //     if (character.weapontype = 1) {
-    //         console.log(`sword and shield`);
-    //     }
-    //     else if (character.weapontype = 2) {
-    //         console.log(`battle axe`); 
-    //     }
-    //     else if (character.weapontype = 3) {
-    //         console.log(`bow and arrow`);
-    //     }   
-    // },
-    
-    
-}
+// const character = { 
+//     name:`${username}`, 
+//     race:`${racetype}`,
+//     armor: null,
+//     weapon: `${weapontype}`,
+//     attack:  0,
+//     defense: 0,
+//     backpack: {
+//         potions: null, //<<--- we'll add a quantity as we expand
+//         arrows: null, //<<--- we'll add a quantity as we eqpand
 
-
+//     }
+// }   
+    
 // console.log(`Your weapon is ` +  ${character.weapon});
-    console.log(character );    
-    
+  
+//=======================================================================================================================
+
 
 
 
@@ -93,6 +134,20 @@ const character = {
 
 //attack and heal functions
 
+// randomeNumGen = () => {
+//     min = Math.ceil(min);
+//     max = MathFloor(max);
+//     return Math.floor(Math.random() * (max - min +1)) +min;
+// }
+
+// let randomNumber = getRandomInt(0, 5);
+// console.log(randomNumber);
+
+
+
+
+
+
 //player turn
 playerturn = () => {
     console.log(`player life is ${playeLife} and computer life is ${computerLife}`)
@@ -101,13 +156,13 @@ playerturn = () => {
         if (playerTurnChoice === `attack`) {
             //attack function lose health from computer
             if (weaponChoice === `1`) {
-                computerLife -= 1 //sword and shield extra defense for the shield 1 attack 2 defence
+                computerLife -= 1 //sword and shield extra defense for the shield +1 attack +2 defence
             }
             else if (weaponChoice === `2`) {
                 computerLife -= 3 // batte axe all attack 3 attack
             }
             else if (weaponChoice === `3`) {
-                computerLife -= 2 //bow gets extra defense due to mobility 2 attack 1 defence
+                computerLife -= 2 //bow gets extra defense due to mobility +2 attack +1 defence
             }
         }
         else if (playerTurnChoice === `heal`) {
@@ -120,8 +175,15 @@ playerturn = () => {
 
     console.log(`player life is ${playeLife} and computer life is ${computerLife}`)
 }
+
+
+evaluate = () => {
+
+}
+
+
 computerturn = () => {
-    //todo with the random math.
+
 
     //computer gets to pick attack or heal
 }
@@ -131,3 +193,4 @@ computerturn = () => {
 
 
 //computer turn
+
