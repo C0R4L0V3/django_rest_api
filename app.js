6f9d2642d0eaf53175c60b0d7c422316f4dd45ab
milestarricone <milestarricone2@gmail.com>
2024-10-21 16:16:01 -0400

    added comments for clarity

--- app.js
+++ app.js
@@ -41,22 +41,22 @@ console.log(`Your chosen race is ${racetype}.`);
 
 
 //sort and shield has a numerical value of 1, battle axe has a numerical value of 2, bow and arrow has a numerical value of 3
-
+//This function is to take imput from the user, and pick a weapon from 3 predetermained weapon choices. 
 console.log(`what weapons do you want to choose?`)
 pickWeapon = () => {
-    const weaponChoice = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`)
-    if (weaponChoice === `1`) {
-        weapontype = `Sword and Shield`
+    const weaponChoice = prompt(`1=sword amd shield, 2=battle axe,.3=bow and arrow.`) //asking the user what they would like to pick
+    if (weaponChoice === `1`) { //comparing the choice the user made to the predetermained selection
+        weapontype = `Sword and Shield`//assigning the weapontype
     }
-    else if (weaponChoice === `2`) {
-        weapontype = `Battle Axe`
+    else if (weaponChoice === `2`) {//comparing the choice the user made to the predetermained selection
+        weapontype = `Battle Axe`//assigning the weapontype
     }
-    else if (weaponChoice === `3`) {
-        weapontype = `Bow and Arrow`
+    else if (weaponChoice === `3`) {//comparing the choice the user made to the predetermained selection
+        weapontype = `Bow and Arrow`//assigning the weapontype
     }
-    else {console.log(`invalid choice, choose better`);
+    else {console.log(`invalid choice, choose better`);//if the user did not pick an option that is predetermained, this calls the function again. 
     weaponChoice}
-    pickWeapon()
+    pickWeapon()//call the function again. 
     
 }
 pickWeapon()
